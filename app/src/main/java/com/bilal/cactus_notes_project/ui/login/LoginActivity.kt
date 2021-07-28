2aed210ec6d14f76cbf83abdf0407dca2e447c78
Bilal-eng <bilalsheikhkhalil@gmail.com>
2021-07-28 11:15:54 +0300

    Create UI elements

--- app/src/main/java/com/bilal/cactus_notes_project/ui/login/LoginActivity.kt
+++ app/src/main/java/com/bilal/cactus_notes_project/ui/login/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.bilal.cactus_notes_project
+package com.bilal.cactus_notes_project.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bilal.cactus_notes_project.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
